4ab49462388581636b382ae696269a0be8a26582
sxxk2 <sxxklee@gmail.com>
2022-06-22 17:36:14 +0900

    :sparkles:Feat: 채용공고 수정(2)

--- recruitings/views.py
+++ recruitings/views.py
@@ -46,3 +46,25 @@ class RecruitingDetailView(View):
             'tech_stack'    : recruiting.tech_stack
         }
         return JsonResponse({'result' : result}, status=200)
+    # 2. 채용공고를 수정합니다.
+    def put(self, request, recruiting_id):
+        try:
+            data = json.loads(request.body)
+
+            position = data['position']
+            reward     = data['reward']
+            content    = data['content']
+            tech_stack = data['tech_stack']
+
+            recruiting = Recruiting.objects.get(id=recruiting_id)
+
+            recruiting.position   = position
+            recruiting.reward     = reward
+            recruiting.content    = content
+            recruiting.tech_stack = tech_stack
+
+            recruiting.save()
+
+            return JsonResponse({'message': 채용공고가 수정되었습니다.}, status=201)
+        except Recruiting.DoesNotExist:
+            return JsonResponse({'message' : '없는 채용공고입니다.'}, status=401)
