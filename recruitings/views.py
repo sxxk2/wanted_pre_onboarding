8de3dd85f82dc5b28596b012c04c297af68279cd
sxxk2 <sxxklee@gmail.com>
2022-06-22 18:24:22 +0900

    :sparkles:Feat: 채용공고 불러오기, 검색(4)

--- recruitings/views.py
+++ recruitings/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.http      import JsonResponse
 from django.views     import View
+from django.db.models import Q
 
 from recruitings.models import Recruiting
 
@@ -28,6 +29,31 @@ class RecruitingView(View):
             return JsonResponse({'message' : '채용공고가 등록되었습니다.'}, status=201)
         except KeyError:
             return JsonResponse({'message': 'KEY ERROR'}, status=400)
+    # 4-1,2. 채용공고를 불러오고 검색합니다.
+    def get(self, request):
+        try:
+            search = request.GET.get('search', None)
+
+            q = Q()
+
+            if search:
+                q &= Q(company__name__icontains=search)
+                q &= Q(position__icontains=search)
+                q &= Q(content__icontains=search)
+                q &= Q(tech_stack__icontains=search)
+
+            results = [{
+                'id'         : recruiting.id,
+                'company'    : recruiting.company.name,
+                'country'    :recruiting.company.country,
+                'region'     :recruiting.company.region,
+                'reward'     : recruiting.reward,
+                'content'    : recruiting.content,
+                'tech_stack' : recruiting.tech_stack,
+            } for recruiting in Recruiting.objects.filter(q).distinct()]
+            return JsonResponse({"results" : results}, status = 200)
+        except KeyError:
+            return JsonResponse({"message" : "KEY ERROR"}, status=400)
 
 class RecruitingDetailView(View):
     # 5. 채용 상세페이지를 가져옵니다.
