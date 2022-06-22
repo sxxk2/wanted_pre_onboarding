9af38cc8baef85867155e3971e988d383f7ea9b9
sxxk2 <sxxklee@gmail.com>
2022-06-22 12:28:49 +0900

    :sparkles:Add: app 생성

--- wanted/settings.py
+++ wanted/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'companies',
+    'recruitings',
 ]
 
 MIDDLEWARE = [
